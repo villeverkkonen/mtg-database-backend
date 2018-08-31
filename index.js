9041a96d7e35063678724f99fe1be015d782c805
Ville Verkkonen <ville@pulmaton.fi>
2018-08-31 16:15:03 +0300

    Some dependencies and testing

--- index.js
+++ index.js
@@ -1,5 +1,53 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(cors())
+
+const logger = (request, response, next) => {
+    console.log('Method:',request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+app.use(logger)
+
+let notes = [
+    {
+      id: 1,
+      content: 'HTML on helppoa',
+      date: '2017-12-10T17:30:31.098Z',
+      important: true
+    },
+    {
+      id: 2,
+      content: 'Selain pystyy suorittamaan vain javascriptiä',
+      date: '2017-12-10T18:39:34.091Z',
+      important: false
+    },
+    {
+      id: 3,
+      content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+      date: '2017-12-10T19:20:14.298Z',
+      important: true
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/notes', (req, res) => {
+    res.json(notes)
+})
+
+const error = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+app.use(error)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
