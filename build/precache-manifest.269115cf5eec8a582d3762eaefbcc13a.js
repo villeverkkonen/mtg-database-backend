6b3050af000c90dffa1a19f27d4051b4f2ce3c4d
Ville Verkkonen <ville@pulmaton.fi>
2018-11-02 11:27:14 +0200

    Added build to repository

---
+++ build/precache-manifest.269115cf5eec8a582d3762eaefbcc13a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1979d13032a10069c168",
+    "url": "/static/js/main.1979d130.chunk.js"
+  },
+  {
+    "revision": "104b6654c4e6ed331ccc",
+    "url": "/static/js/1.104b6654.chunk.js"
+  },
+  {
+    "revision": "1979d13032a10069c168",
+    "url": "/static/css/main.252c025b.chunk.css"
+  },
+  {
+    "revision": "c22071152064fde2b8cae3d406a0b13e",
+    "url": "/index.html"
+  }
+];
